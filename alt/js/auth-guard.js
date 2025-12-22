6b1cc1bbc95c1a691b1a180f4ef91a2bbf9e982a
Vinícius Miiller Rebello <miiller.vini@gmail.com>
2025-12-22 16:21:01 -0300

    Redirect unauthenticated users to alt/login.html

---
+++ alt/js/auth-guard.js
@@ -0,0 +1,10 @@
+<script type="module">
+import { auth } from "./firebase.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+onAuthStateChanged(auth, (user) => {
+  if (!user) {
+    window.location.href = "alt/login.html";
+  }
+});
+</script>
