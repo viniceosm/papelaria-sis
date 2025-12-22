524a185ca35f66d3c6804e148f61714b78f889e9
Vinícius Miiller Rebello <miiller.vini@gmail.com>
2025-12-22 16:19:56 -0300

    Add Firebase configuration and initialization script

---
+++ alt/js/firebase.js
@@ -0,0 +1,18 @@
+<script type="module">
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBjvTgvMxOiUcJzud6Vapa5X8ZnGeph0aA",
+  authDomain: "papelaria-79dc5.firebaseapp.com",
+  projectId: "papelaria-79dc5",
+  storageBucket: "papelaria-79dc5.firebasestorage.app",
+  messagingSenderId: "730323947354",
+  appId: "1:730323947354:web:6a61e2312a6cc4e361ad63"
+};
+
+export const app  = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db   = getFirestore(app);
+</script>
